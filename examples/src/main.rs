e2b612c722f78bc8cb559bc74270bd79c0607bbe
bombless <bombless@126.com>
2015-01-10 13:48:32 +0800

    fit general coding style

--- examples/src/main.rs
+++ examples/src/main.rs
@@ -3,9 +3,9 @@ use paint::{ Layer, Canvas, Projection, Color, Size };
 
 struct BigWhiteCircle;
 
-impl Layer for BigWhiteCircle{
-	fn draw(&self, projection :Projection)->Color{
-		use std::cmp::{ max, min };
+impl Layer for BigWhiteCircle {
+	fn draw(&self, projection: Projection)->Color {
+		use std::cmp:: { max, min };
 		let origin_x = std::u32::MAX / 2;
 		let offset_x = max(projection.x, origin_x) - min(projection.x, origin_x);
 		let origin_y = std::u32::MAX / 2;
@@ -13,9 +13,9 @@ impl Layer for BigWhiteCircle{
 		let offset_x = offset_x as f64;
 		let offset_y = offset_y as f64;
 		let dis = std::num::Float::sqrt(offset_x * offset_x + offset_y * offset_y);
-		if dis < (std::u32::MAX / 2) as f64{
+		if dis < (std::u32::MAX / 2) as f64 {
 			Color::rgb(255, 255, 255)
-		}else{
+		} else {
 			Color::transparent()
 		}
 	}
@@ -23,57 +23,57 @@ impl Layer for BigWhiteCircle{
 
 struct Position(f32, f32, f32);
 
-struct Light{
-	origin :Position,
+struct Light {
+	origin: Position,
 }
 
-struct Ball{
-	origin :Position,
-	radius :f32
+struct Ball {
+	origin: Position,
+	radius: f32
 }
 
-struct Scene{
-	light :Light,
-	ball :Ball,
-	eye :Position
+struct Scene {
+	light: Light,
+	ball: Ball,
+	eye: Position
 }
 
-impl Scene{
-	fn new()->Scene{
-		let ball = Ball{ origin: Position(3f32, 30f32, 3f32), radius: 3f32 };
-		let light = Light{ origin: Position(0f32, 0f32, 0f32) };
+impl Scene {
+	fn new()->Scene {
+		let ball = Ball { origin: Position(3f32, 30f32, 3f32), radius: 3f32 };
+		let light = Light { origin: Position(0f32, 0f32, 0f32) };
 		let eye = Position(3f32, 0f32, 3f32);
-		Scene{ ball: ball, light: light, eye: eye }
+		Scene { ball: ball, light: light, eye: eye }
 	}
 }
 
-struct TwoLayer<U :Layer, V :Layer>{
-	a :U, b :V
+struct TwoLayer<U: Layer, V: Layer> {
+	a: U, b: V
 }
 
-impl<U :Layer, V :Layer> Layer for TwoLayer<U, V>{
-	fn draw(&self, projection :Projection)->Color{
+impl<U: Layer, V: Layer> Layer for TwoLayer<U, V> {
+	fn draw(&self, projection: Projection)->Color {
 		projection.proxy_split(|p|self.a.draw(p), |p|self.b.draw(p))
 	}
 }
 
-fn main(){
+fn main() {
 	let args = std::os::args();
-	if args.len() == 2{
-		match std::path::Path::new_opt(&*args[1]){
-			Some(ref path) =>{
-				match std::io::fs::File::create(path){
-					Ok(ref mut file) =>{
+	if args.len() == 2 {
+		match std::path::Path::new_opt(&*args[1]) {
+			Some(ref path) => {
+				match std::io::fs::File::create(path) {
+					Ok(ref mut file) => {
 						let canvas = Canvas::new(Color::rgb(0, 200, 0), Size::new(260, 520));
-						let layer = TwoLayer{ a: BigWhiteCircle, b: BigWhiteCircle };
+						let layer = TwoLayer { a: BigWhiteCircle, b: BigWhiteCircle };
 						canvas.render(layer, file);
 					},
-					Err(x)=>println!("{}", x)
+					Err(x)=>println!(" {}", x)
 				}
 			},
 			None =>println!("failed to open path")
 		}
-	}else{
+	} else {
 		println!("Usage: {} file_path \t write a ppm file", &*args[0])
 	}
 }
--- src/paint.rs
+++ src/paint.rs
@@ -1,31 +1,31 @@
 use std::io::Writer;
 
 #[derive(Copy)]
-pub struct Size{
-	width :u32,
-	height :u32
+pub struct Size {
+	width: u32,
+	height: u32
 }
 
-impl Size{
-	pub fn new(w :u32, h :u32)->Size{
-		Size{ width: w, height: h }
+impl Size {
+	pub fn new(w: u32, h: u32)->Size {
+		Size { width: w, height: h }
 	}
 }
 
 #[derive(Copy)]
-pub struct Position{
-	x :f32,
-	y :f32
+pub struct Position {
+	x: f32,
+	y: f32
 }
 
-impl Position{
-	pub fn new(x :u32, y:u32)->Position{
-		Position{ x: x as f32, y: y as f32 }
+impl Position {
+	pub fn new(x: u32, y: u32)->Position {
+		Position { x: x as f32, y: y as f32 }
 	}
 	
-	pub fn distance(&self, point :Position)->f32{
+	pub fn distance(&self, point: Position)->f32 {
 		use std::num::Float;
-		let square = |&: x :f32| x * x;
+		let square = |&: x: f32| x * x;
 		let offset_x = self.x - point.x;
 		let offset_y = self.y - point.y;
 		Float::sqrt(square(offset_x) + square(offset_y))
@@ -35,118 +35,119 @@ impl Position{
 #[derive(PartialEq, Copy)]
 pub struct Color(f32, f32, f32, f32);
 
-impl Color{
-	fn f32_to_u8(v :f32)->u8{
+impl Color {
+	fn f32_to_u8(v: f32)->u8 {
 		use std::num::ToPrimitive;
-		match ToPrimitive::to_u8(&(v * 256f32)){
+		match ToPrimitive::to_u8(&(v * 256f32)) {
 			Some(x) =>x,
-			None =>if v > 0.5f32{ 255 }else{ 0 }
+			None =>if v > 0.5f32 { 255 } else { 0 }
 		}		
 	}
 	
-	pub fn r(&self)->u8{ Color::f32_to_u8(self.0) }
+	pub fn r(&self)->u8 { Color::f32_to_u8(self.0) }
 	
-	pub fn g(&self)->u8{ Color::f32_to_u8(self.1) }
+	pub fn g(&self)->u8 { Color::f32_to_u8(self.1) }
 	
-	pub fn b(&self)->u8{ Color::f32_to_u8(self.2) }
+	pub fn b(&self)->u8 { Color::f32_to_u8(self.2) }
 	
-	pub fn rgb(r :u8, g :u8, b :u8)->Color{
+	pub fn rgb(r: u8, g: u8, b: u8)->Color {
 		Color::rgba(r, g, b, 255)
 	}
 	
-	pub fn rgba(r :u8, g :u8, b :u8, a :u8)->Color{
+	pub fn rgba(r: u8, g: u8, b: u8, a: u8)->Color {
 		Color(r as f32 / 256f32, g as f32 / 256f32, b as f32 / 256f32, a as f32 / 256f32)
 	}
 	
-	pub fn transparent()->Color{
+	pub fn transparent()->Color {
 		Color(1f32, 1f32, 1f32, 0f32)
 	}
 	
-	pub fn over(&self, dst :Color)->Color{
+	pub fn over(&self, dst: Color)->Color {
 		let &Color(lhs_r, lhs_g, lhs_b, lhs_a) = self;
 		let Color(rhs_r, rhs_g, rhs_b, rhs_a) = dst;
 		let r = lhs_r * lhs_a + rhs_r * rhs_a * (1f32 - lhs_a);
 		let g = lhs_g * lhs_a + rhs_g * rhs_a * (1f32 - lhs_a);
 		let b = lhs_b * lhs_a + rhs_b * rhs_a * (1f32 - lhs_a);
-		let a = if lhs_a > rhs_a { lhs_a }else { rhs_a };
+		let a = if lhs_a > rhs_a { lhs_a } else { rhs_a };
 		Color(r, g, b, a)
 	}
 }
 
 #[derive(Copy)]
-pub struct Projection{
-	pub x :u32,
-	pub y :u32
+pub struct Projection {
+	pub x: u32,
+	pub y: u32
 }
 
-impl Projection{
-	fn f64_to_u32(v :f64)->u32{
+impl Projection {
+	fn f64_to_u32(v: f64)->u32 {
 		use std::u32;
 		use std::f64;
 		use std::num::ToPrimitive;
-		match ToPrimitive::to_u32(&v){
+		match ToPrimitive::to_u32(&v) {
 			Some(v) =>v,
-			None =>if v > f64::MAX_VALUE / 2f64{
+			None =>if v > f64::MAX_VALUE / 2f64 {
 				u32::MAX
-			}else{
+			} else {
 				0
 			}
 		}
 	}
 	
-	pub fn proxy_split<F1 :Fn(Projection)->Color, F2 :Fn(Projection)->Color>(&self, f1 :F1, f2 :F2)->Color{
+	pub fn proxy_split<F1, F2>(&self, f1: F1, f2: F2)->Color
+		where F1: Fn(Projection)->Color, F2: Fn(Projection)->Color {
 		use std::u32;
 		use std::num::ToPrimitive;
 		let mid_value = (u32::MAX / 2) as f64;
-		let (value, latter) = if self.y > u32::MAX / 2{
+		let (value, latter) = if self.y > u32::MAX / 2 {
 			(2f64 * (self.y as f64 - mid_value), true)
-		}else{
+		} else {
 			(2f64 * self.y as f64, false)
 		};
-		let p = match ToPrimitive::to_u32(&value){
-			Some(y) =>Projection{ x: self.x, y: y },
-			None =>Projection{ x: self.x, y: if value > mid_value{
+		let p = match ToPrimitive::to_u32(&value) {
+			Some(y) =>Projection { x: self.x, y: y },
+			None =>Projection { x: self.x, y: if value > mid_value {
 					u32::MAX
-				}else{
+				} else {
 					0
 				}
 			}
 		};
-		if latter{ f2(p) }else{ f1(p) }
+		if latter { f2(p) } else { f1(p) }
 	}
 	
-	pub fn new(canvas_size :&Size, position :Position)->Projection{
+	pub fn new(canvas_size: &Size, position: Position)->Projection {
 		use std::u32;
 		let relative_x = position.x as f64 / canvas_size.width as f64;
 		let x = Projection::f64_to_u32(relative_x * u32::MAX as f64);
 		let relative_y = position.y as f64 / canvas_size.height as f64;
 		let y = Projection::f64_to_u32(relative_y * u32::MAX as f64);
-		Projection{ x: x, y: y }
+		Projection { x: x, y: y }
 	}
 }
 
-pub trait Layer{
+pub trait Layer {
 	fn draw(&self, Projection)->Color;
 }
 
 #[derive(Copy)]
-pub struct Canvas{
-	background :Color,
-	size :Size
+pub struct Canvas {
+	background: Color,
+	size: Size
 }
 
-impl Canvas{
-	pub fn new(background :Color, s :Size)->Canvas{
-		Canvas{ size: s, background: background }
+impl Canvas {
+	pub fn new(background: Color, s: Size)->Canvas {
+		Canvas { size: s, background: background }
 	}
 	
-	pub fn render<T :Layer>(&self, layer :T, writer :&mut Writer){
+	pub fn render<T: Layer>(&self, layer: T, writer: &mut Writer) {
 		let mut canvas = Vec::new();
-		for _ in 0 .. self.size.width * self.size.height{
+		for _ in 0 .. self.size.width * self.size.height {
 			canvas.push(self.background)
 		}
-		for x in 0 .. self.size.width{
-			for y in 0 .. self.size.height{
+		for x in 0 .. self.size.width {
+			for y in 0 .. self.size.height {
 				let index = x as usize + self.size.width as usize * y as usize;
 				let projection = Projection::new(&self.size, Position::new(x, y));
 				canvas[index] = layer.draw(projection).over(canvas[index])
@@ -155,7 +156,7 @@ impl Canvas{
 		writer.write_line("P3").unwrap();
 		writer.write_line(&*format!("{} {}", self.size.width, self.size.height)).unwrap();
 		writer.write_line("255").unwrap();
-		for i in canvas.iter(){
+		for i in canvas.iter() {
 			writer.write_line(&*format!("{} {} {}", i.r(), i.g(), i.b())).unwrap();
 		}
 	}
